15cb92306d32fd50cddd46470b09012028be6f3c
dhazlettjr <d.hazlett.jr@gmail.com>
2018-08-15 22:45:06 -0500

    added css, animation, button click

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ $(document).ready(function(){
     if(city != ''){
 
       $.ajax({
-        url:'http://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=metric" + "&APPID=7646dc8cdac6461b5b997f33f0a9c975",
+        url:'http://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=imperial" + "&APPID=7646dc8cdac6461b5b997f33f0a9c975",
         type: "GET",
         dataType: "jsonp",
         success: function(data){
+            $('#show').append(JSON.stringify(data.main.temp))
           console.log(data);
         }
       })
