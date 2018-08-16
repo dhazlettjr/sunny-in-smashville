d7efece4511645a41197867630600ff7d5a8d1de
David A Hazlett Jr <d.hazlett.jr@gmail.com>
2018-08-15 22:54:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     if(city != ''){
 
       $.ajax({
-        url:'http://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=imperial" + "&APPID=7646dc8cdac6461b5b997f33f0a9c975",
+        url:'https://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=imperial" + "&APPID=7646dc8cdac6461b5b997f33f0a9c975",
         type: "GET",
         dataType: "jsonp",
         success: function(data){
